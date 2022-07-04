74e020f40c25225b5c6538726d3d6aee194dcd47
ZeroM2 <kemy21@naver.com>
2022-07-04 22:21:38 +0900

    count 조절액션구현,쿼리스트링

--- src/main/java/com/lmy/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/lmy/exam/demo/controller/UserHomeController.java
@@ -8,36 +8,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserHomeController {
 	private int count;  //전역변수 count 선언 
 	public UserHomeController() { //생성자 
-		count = 0;  // 
+		count = -1;
 	}
-	@RequestMapping("/user/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	} 
-	
-		@RequestMapping("/user/home/main2")
+
+
+		@RequestMapping("/user/home/getCount")
 		@ResponseBody
-		public String showMain2() {
-			return "반갑습니다";
-		}
-		@RequestMapping("/user/home/main3")
-		@ResponseBody
-		public String showMain3() {
-			return "또 만나요오";
-		}
-		@RequestMapping("/user/home/main4")
-		@ResponseBody
-		public int showMain4() {
-			return count++; //일단 0의 값을 리턴하고 카운트에  1을 더한다 
-		}  
-		@RequestMapping("/user/home/main5")
-		@ResponseBody
-		public String showMain5() {
-			count = 0;
-			return "count의 값이 초기화되었습니다."; //일단 0의 값을 리턴하고 카운트에  1을 더한다 
-			
+		public int getCount() {
 		
+			return count; 
 		}  
+		@RequestMapping("/user/home/doSetCount")
+		@ResponseBody
+		public String doSetcount(int count){
+			this.count = count;
+			return "count의 값이" + this.count + "초기화되었습니다.";
+				
+			
+		}  
+
 
 }
