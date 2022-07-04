439017adbcc7103038f51d77d75560b5b1e61284
ZeroM2 <kemy21@naver.com>
2022-07-04 23:27:06 +0900

    각종리턴타입 test

--- src/main/java/com/lmy/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/lmy/exam/demo/controller/UserHomeController.java
@@ -1,31 +1,84 @@
 package com.lmy.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-	private int count;  //전역변수 count 선언 
-	public UserHomeController() { //생성자 
-		count = -1;
-	}
-
-
-		@RequestMapping("/user/home/getCount")
+		@RequestMapping("/user/home/getString")
 		@ResponseBody
-		public int getCount() {
+		public String getString() {
 		
-			return count; 
-		}  
-		@RequestMapping("/user/home/doSetCount")
-		@ResponseBody
-		public String doSetcount(int count){
-			this.count = count;
-			return "count의 값이" + this.count + "초기화되었습니다.";
-				
+			return "hi";
 			
-		}  
-
-
 }
+
+
+		@RequestMapping("/user/home/getInt")
+		@ResponseBody
+		public int getint() {
+		
+			return 10;
+		}
+
+		@RequestMapping("/user/home/getFloat")
+		@ResponseBody
+		public float getFloat() {
+		
+			return 1.5f;
+		}
+
+		@RequestMapping("/user/home/getDouble")
+		@ResponseBody
+		public double getDouble() {
+		
+			return 1.5;
+		}
+
+		@RequestMapping("/user/home/getBoolean")
+		@ResponseBody
+		public boolean getBoolean() {
+		
+			return true;
+		}
+		@RequestMapping("/user/home/getCharacter")
+		@ResponseBody
+		public char getCharacter() {
+		
+			return 'a';
+		}
+		@RequestMapping("/user/home/getList")
+		@ResponseBody
+		public List<String> getList() {
+			List<String> list = new ArrayList<>();
+			list.add("철수");
+			list.add("영희");
+			
+			return list;
+		}
+		@RequestMapping("/user/home/getArticle")
+		@ResponseBody
+		public Article getArticle() {
+			Article article = new Article();
+			return article;
+		}
+		
+		
+}
+
+class Article {
+	public int id;
+	public String title;
+	
+	public Article() {
+		id = 1;
+		title = "제목1";
+	}
+	
+} 
